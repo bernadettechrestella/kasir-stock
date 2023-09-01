e5eff440969f39c472efbd883becac467d7a29b0
bernadettechrestella <bernadettechrestella@gmail.com>
2023-09-01 13:51:29 +0900

    Create auth.services.js

---
+++ src/services/auth.services.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+import jwtDecode from "jwt-decode";
+
+export const login = (data, callback) => {
+    axios
+        .post('https://dummyjson.com/auth/login', data)
+        .then((res) => {
+            callback(true, res.data.token);
+        })
+        .catch((err) => {
+            callback(false, err)
+        })
+}
+
+export const getFirstName = (token) => {
+    const decoded = jwtDecode(token);
+    return decoded.firstName
+}
