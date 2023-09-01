669ff09cb5778149035559522e86da522474cfb1
bernadettechrestella <bernadettechrestella@gmail.com>
2023-09-01 13:51:17 +0900

    Create useLogin.jsx

---
+++ src/hooks/useLogin.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { getFirstName } from "../services/auth.services";
+
+export default function useLogin() {
+    const [firstName, setFirstName] = useState("");
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            setFirstName(getFirstName(token));
+        } else {
+            window.location.href = "/login";
+        }
+        setFirstName(getFirstName(token))
+    }, []);
+
+    console.log("uselogin")
+
+    return firstName;
+}
