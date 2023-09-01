a6eb10d54bccebb1563200f7884e3f59a658aa7f
bernadettechrestella <bernadettechrestella@gmail.com>
2023-09-01 13:51:11 +0900

    Update FormLogin.jsx

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import InputForm from '../Elements/Input/InputForm'
+import { login } from '../../services/auth.services';
 
 const FormLogin = () => {
+  const [loginFailed, setLoginFailed] = useState('');
+  const handleLogin = (e) => {
+    e.preventDefault(); //prevent default agar tidak reload halaman
+
+    const data = {
+      username: e.target.username.value,
+      password: e.target.password.value,
+    }
+
+    login(data, (status, res) => {
+      if (status) {
+        localStorage.setItem("token", res);
+        window.location.href = "/kasir"
+      } else {
+        setLoginFailed(res.response.data)
+        console.log(res.response.data);
+      }
+    })
+  }
+
+  const usernameRef = useRef(null);
+
+  useEffect(() => {
+    usernameRef.current.focus();
+  }, [])
+
   return (
-    <form>
+    <form onSubmit={handleLogin}>
         <InputForm 
             label="Username"
             type="text"
             placeholder="John Doe"
             name="username"
-            ref={username}
+            ref={usernameRef}
         />
         <InputForm 
             label="Password"
@@ -17,6 +44,8 @@ const FormLogin = () => {
             placeholder="********"
             name="password"
         />
+        <button className='bg-cyan-800 text-white font-bold rounded w-full h-10' type='submit'>Log In</button>
+        {loginFailed && <div className="text-red-500 font-medium text-center mt-5">{loginFailed}</div>}
     </form>
   )
 }
