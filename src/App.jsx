3207c93be9aed3c2ff4ea0dd0655a38693e63484
bernadettechrestella <bernadettechrestella@gmail.com>
2023-09-06 14:26:13 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import { Link } from "react-router-dom"
+import {CgProfile} from 'react-icons/cg'
+import {GiReceiveMoney} from 'react-icons/gi'
+import {TbFileInvoice} from 'react-icons/tb'
+import {BsFillInboxesFill} from 'react-icons/bs'
 
 function App() {
   return (
-    <div className="text-center">
-      <Link to='/kasir'>
-        <button>Kasir</button>
-      </Link>
+    <div className='flex justify-center bg-cyan-100 min-h-screen items-center'>
+      <div className="w-full max-w-xs">
+        <div className="flex text-cyan-800 gap-10">
+          <div className="text-center">
+            <Link to="/kasir">
+              <GiReceiveMoney size={40} className='m-1'/>
+              <h1>Kasir</h1>
+            </Link>
+          </div>
+          <div className="text-center">
+            <Link to="/">
+              <TbFileInvoice size={40} className='m-1'/>
+              <h1>Nota</h1>
+            </Link>
+          </div>
+          <div className="text-center">
+            <Link to="/stock">
+              <BsFillInboxesFill size={40} className='m-1'/>
+              <h1>Stock</h1>
+            </Link>
+          </div>
+          <div className="text-center">
+            <Link to="/">
+              <CgProfile size={40} className='m-1'/>
+              <h1>User</h1>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
