d1ccb3a7845d8811dd5322f3a152e2a1cff6b6bd
bernadettechrestella <bernadettechrestella@gmail.com>
2023-09-16 12:07:10 +0900

    Update useLogin.jsx

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react";
-import { getFirstName } from "../services/auth.services";
+import { getFirstName, getUserID } from "../services/auth.services";
 
 export default function useLogin() {
     const [firstName, setFirstName] = useState("");
+    const [userID, setUserID] = useState()
 
     useEffect(() => {
         const token = localStorage.getItem('token');
 
         if (token) {
             setFirstName(getFirstName(token));
+            setUserID(getUserID(token));
         } else {
             window.location.href = "/login";
         }
         setFirstName(getFirstName(token))
+        setUserID(getUserID(token))
     }, []);
 
-    return firstName;
+    return {firstName, userID}
 }
