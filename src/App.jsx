e676fa4f9d65858e670826f94e3228fe211954a4
bernadettechrestella <bernadettechrestella@gmail.com>
2023-09-19 11:44:48 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,25 @@ import {BsFillInboxesFill} from 'react-icons/bs'
 
 function App() {
   return (
-    <div className='flex justify-center bg-cyan-100 min-h-screen items-center'>
-      <div className="w-full max-w-xs flex flex-col">
-        <div className="flex text-cyan-800 gap-10">
+    <div className="flex justify-center items-center min-h-screen bg-[url('/public/images/bg.jpg')] bg-cover">
+      <div className="w-full max-w-md bg-white bg-opacity-70 rounded-full">
+        <div className="text-cyan-900 font-bold flex justify-around">
           <div className="text-center">
-            <Link to="/kasir">
+            <p>Use any Account from</p>
+            <a href="https://dummyjson.com/docs/users" className="text-blue-700">https://dummyjson.com/docs/users</a>
+            <p>for Login</p>
+          </div>
+          <div className="text-center animate-bounce">
+          <Link to="/kasir">
               <GiReceiveMoney size={40} className='m-1'/>
               <h1>Kasir</h1>
             </Link>
           </div>
-          <div className="text-center">
-            <Link to="/">
-              <TbFileInvoice size={40} className='m-1'/>
-              <h1>Nota</h1>
-            </Link>
-          </div>
-          <div className="text-center">
-            <Link to="/stock">
-              <BsFillInboxesFill size={40} className='m-1'/>
-              <h1>Stock</h1>
-            </Link>
-          </div>
-          <div className="text-center">
-            <Link to="/user">
-              <CgProfile size={40} className='m-1'/>
-              <h1>User</h1>
-            </Link>
-          </div>
         </div>
-        <div className="bottom-0 absolute mb-2">
-          <h1 className="text-cyan-800 font-bold font-mono">Made by Bernadette Chrestella</h1>
+        <div className="flex-grow">
+          <div className="bottom-0 absolute mb-2 w-full max-w-md bg-white bg-opacity-70 rounded-full">
+            <h1 className="text-cyan-900 font-bold font-mono text-center">Made by Bernadette Chrestella</h1>
+          </div>
         </div>
       </div>
     </div>
